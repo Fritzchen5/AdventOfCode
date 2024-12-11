aa64fabf9d02aa3817a549c1d8e7502f46bd0ffb
Tamino Larisch <tamino.lar@gmail.com>
2024-12-11 09:47:26 +0100

    Day11 with BigInt

--- src/aoc2024/Day11.java
+++ src/aoc2024/Day11.java
@@ -13,11 +13,11 @@ public class Day11 {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line = reader.readLine();
             ArrayList<BigInteger> stones = new ArrayList<>();
-            HashMap<BigInteger, Long> map = new HashMap<>();
+            HashMap<BigInteger, BigInteger> map = new HashMap<>();
             for (String s : line.split("\\s")) {
                 BigInteger i = new BigInteger(s);
                 stones.add(i);
-                map.put(i, map.getOrDefault(i, 0L) + 1);
+                map.put(i, map.getOrDefault(i, BigInteger.ZERO).add(BigInteger.ONE));
             }
 
             for (int i = 0; i < 25; i++) {
@@ -37,28 +37,28 @@ public class Day11 {
             System.out.println(result1);
 
             for (int i = 0; i < 75; i++) {
-                HashMap<BigInteger, Long> newMap = new HashMap<>();
-                for (Map.Entry<BigInteger, Long> set : map.entrySet()) {
+                HashMap<BigInteger, BigInteger> newMap = new HashMap<>();
+                for (Map.Entry<BigInteger, BigInteger> set : map.entrySet()) {
                     BigInteger k = set.getKey();
                     String s = k.toString();
-                    long v = set.getValue();
+                    BigInteger v = set.getValue();
                     if (Objects.equals(s, "0"))
-                        newMap.put(BigInteger.ONE, newMap.getOrDefault(BigInteger.ONE, 0L) + v);
+                        newMap.put(BigInteger.ONE, newMap.getOrDefault(BigInteger.ONE, BigInteger.ZERO).add(v));
                     else if (s.length() % 2 == 0) {
                         BigInteger n1 = new BigInteger(s.substring(0, s.length() / 2));
                         BigInteger n2 = new BigInteger(s.substring(s.length() / 2));
-                        newMap.put(n1, newMap.getOrDefault(n1, 0L) + v);
-                        newMap.put(n2, newMap.getOrDefault(n2, 0L) + v);
+                        newMap.put(n1, newMap.getOrDefault(n1, BigInteger.ZERO).add(v));
+                        newMap.put(n2, newMap.getOrDefault(n2, BigInteger.ZERO).add(v));
                     } else {
                         BigInteger n = k.multiply(BigInteger.valueOf(2024));
-                        newMap.put(n, newMap.getOrDefault(n, 0L) + v);
+                        newMap.put(n, newMap.getOrDefault(n, BigInteger.ZERO).add(v));
                     }
                 }
                 map = newMap;
             }
-            long result2 = 0;
-            for (Map.Entry<BigInteger, Long> set : map.entrySet())
-                result2 += set.getValue();
+            BigInteger result2 = BigInteger.ZERO;
+            for (Map.Entry<BigInteger, BigInteger> set : map.entrySet())
+                result2 = result2.add(set.getValue());
             System.out.println(result2);
         } catch (
                 IOException e) {
